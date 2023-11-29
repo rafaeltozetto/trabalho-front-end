8ff17ca2d0de6ec6fad52c8875166467809fe5a4
rafaeltozetto <132474935+rafaeltozetto@users.noreply.github.com>
2023-11-29 15:44:13 -0300

    Add files via upload

---
+++ Nova pasta/index.js
@@ -0,0 +1,5 @@
+javascript
+function toggleMenu() {
+    var menuList = document.getElementById("menuList");
+    menuList.style.display = (menuList.style.display === "block") ? "none" : "block";
+}
